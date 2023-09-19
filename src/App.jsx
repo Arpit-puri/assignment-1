f5826af749c6d6328713f4334c0a418e357d4440
Abhi-atg-1021 <abhisinghal112@gmail.com>
2023-09-19 20:54:31 +0530

    changes:

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Layout from './components/shared/Layout'
-import Register from './pages/Register'
-import Dashboard from './pages/Dashboard'
-import Products from './pages/Products'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/shared/Layout';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import Products from './pages/Products';
+import { createContext, useState } from 'react';
+
+export const UserContext = createContext(null);
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Dashboard />} />
-                    <Route path="products" element={<Products />} />
-                </Route>
-                <Route path="/register" element={<Register />} />
-            </Routes>
-        </Router>
-    )
+  // Define your initial context value here
+  const [user, setUser] = useState([]);
+
+  return (
+    <Router>
+      <UserContext.Provider value={{user,setUser}}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="products" element={<Products />} />
+          </Route>
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </UserContext.Provider>
+    </Router>
+  );
 }
 
-export default App
+export default App;
